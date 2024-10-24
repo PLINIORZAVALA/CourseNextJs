117ac1a8247fc0394d529b9b978bb457bb362254
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-10-24 02:26:54 -0500

    0.8 Nested Dynamic Routes

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,5 +1,5 @@
 export default function ProductId({params}: {
     params : {productId: string};
 }) {
-    return <h1> Detail to page Ids! {params.productId}</h1>;
+    return <h1> Detail to page Ids: {params.productId}</h1>;
 }
