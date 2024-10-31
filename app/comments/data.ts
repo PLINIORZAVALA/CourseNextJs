2d0ee3ad9d3b1425466594c1bb2fb60072e8bb2d
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-10-31 02:57:31 -0500

    3.4. Hadling GET Request

---
+++ app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id : 1, 
+        text: "This is the first commet",
+    },
+    {
+        id : 2, 
+        text: "This is the second commet",
+    },
+    {
+        id : 3, 
+        text: "This is the third commet",
+    },
+];
