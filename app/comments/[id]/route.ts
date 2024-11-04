686ee7883795dbeb303fef418ba92a47868b3b71
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-11-04 03:01:54 -0500

    Handling PATCH Request

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -17,3 +17,26 @@ export async function GET(
         status: 200,
     });
 }
+
+export async function PATCH(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+    const body = await request.json();
+    const { text } = body;
+
+    const index = comments.findIndex(
+        (comment) => comment.id === parseInt(params.id)
+    );
+
+    if (index === -1) {
+        return new Response("Comment not found", { status: 404 });
+    }
+
+    comments[index].text = text;
+
+    return new Response(JSON.stringify(comments[index]), {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+    });
+}
