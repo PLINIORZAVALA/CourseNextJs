1a0511a4b60be86e86609cfbcc8355220c680cd3
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-11-04 02:31:31 -0500

    3.5. Handling POST Request

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -3,3 +3,19 @@ import { comments } from "./data";
 export async function GET() {
     return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+        const commet = await request.json()
+
+        const newcommet = {
+            id : comments.length + 1,
+            text: commet.text
+        };
+        comments.push(newcommet)
+    return new Response(JSON.stringify(newcommet),{
+        headers: {
+            "Content-Type": "application/json",
+        },
+        status: 201
+    });
+}
